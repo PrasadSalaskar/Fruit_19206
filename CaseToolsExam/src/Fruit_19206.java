01d170d939ed84fd3362d80bafc0876756dc526f
Arti <Arti@LAPTOP-FFL5Q5BN>
2021-07-23 14:11:57 +0530

    hello

---
+++ CaseToolsExam/src/Fruit_19206.java
@@ -0,0 +1,14 @@
+
+public class Fruit_19206 {
+String color;
+	Fruit_19206(String color){
+		this.color=color;
+	}
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
